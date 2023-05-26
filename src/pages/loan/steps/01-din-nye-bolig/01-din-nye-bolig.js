9cb4e9d9c993a8eb47536e290d0b0230114ff737
José <sesejose@gmail.com>
2023-05-26 21:46:06 +0200

    Modal Intro

--- src/pages/loan/steps/01-din-nye-bolig/01-din-nye-bolig.js
+++ src/pages/loan/steps/01-din-nye-bolig/01-din-nye-bolig.js
@@ -34,6 +34,83 @@ export default function DinNyeBolig(props) {
 
   return (
     <>
+      {/* Modal intro  */}
+      <div id="modal-intro" className="grid-center">
+        <div className="modal-container flex-column-center col-2-12">
+          <h2>Goft at vide inden du går i gang med din ansøgning</h2>
+          <p className="small">
+            I gennem ansøgning process vi kommer at pørger dig om forskællige ting, bland andet den bolig du/I ønsker at købe, din/jeres boligsituation og hvis du/I har gælder i form af boliglån, realkreditlån, banklån eller SU lån på din/jeres
+            nuværende ejersbolig hvis der er en. Vi kommer også at spørger om husholdindkomster, opsparinger og andre gæld som kassekredit og banklån. Så anbefælles at have disse informationer inden du går i gang for at fylde fælterne ind.
+          </p>
+          <br></br>
+          <p className="small">Her en liste med hvad kommer vi at spørger dig:</p>
+
+          <div className="flex-row-modal">
+            <ul className="bullet">
+              <li>
+                <strong>Boligsituation</strong>
+              </li>
+              <li>Boligen type</li>
+              <li>Boligen adresse</li>
+              <li>Boligen værd</li>
+              <li>Boliglån type (hvis der er)</li>
+              <li>Boliglån restgæld, rente og rente type (hvis der er)</li>
+            </ul>
+            <ul className="bullet">
+              <li>
+                <strong>Ejendele</strong>
+              </li>
+              <li>Køretøj mærke, model, årgang og ejerforholdene</li>
+              <li>Køretøj lån (hvis der er)</li>
+              <li>Køretøj låneudbyder, skylder og månedlige ydelse</li>
+            </ul>
+            <ul className="bullet">
+              <li>
+                <strong>Indtægter</strong>
+              </li>
+              <li>Månedlig husstandsindkomst (før skat)</li>
+              <li>Arbejdsform og månedlige indkomst</li>
+              <li>Fagforening (hvis der er)</li>
+              <li>A-kasse (hvis der er)</li>
+            </ul>
+            <ul className="bullet">
+              <li>
+                <strong>Gæld</strong>
+              </li>
+              <li>SU lån (hvis der er)</li>
+              <li>Banklån (hvis der er)</li>
+              <li>Banklån udbyder</li>
+              <li>Restgæld på lån</li>
+              <li>Rentesats på lån</li>
+              <li>Rentetype og månedlige ydelse</li>
+            </ul>
+            <ul className="bullet">
+              <li>
+                <strong>Opsparing</strong>
+              </li>
+              <li>Din bank</li>
+              <li>Husstanden stående på opsparings og lønkonti</li>
+              <li>Renten på opsparingskontoen</li>
+              <li>Kassekredit (hvis der er)</li>
+              <li>Kassekreditten maksimale beløb, penge på den og rente</li>
+              <li>Aktier (hvis der er) og penge i den</li>
+              <li>Penge i private pensionsopsparing (hvis der er)</li>
+            </ul>
+          </div>
+
+          <p className="small">Det er også en god idé at oprette en konto og log ind, så hvis du skal forlade vi gemmer alle informationer du har allerede indstat for når du kommer tilbage.</p>
+          <p>God fornjølse</p>
+          <div className="flex-row-center">
+            <button className="btn-contact" onClick={hideModalIntro}>
+              Fortsæt uden logge ind
+            </button>
+            <Link className="btn-contact" href={""}>
+              Opret en konto
+            </Link>
+          </div>
+        </div>
+      </div>
+
       {/* Back and Steps Bar */}
       <div className="step-back-wrapper">
         <div className="flex-row-space-around step-back-container">
@@ -122,6 +199,10 @@ function openSteps() {
   } else {
     steps.style.display = "flex";
   }
+}
+
+function hideModalIntro() {
+  document.querySelector("#modal-intro").classList.add("hiden");
 }
 
 // function submit(e) {
